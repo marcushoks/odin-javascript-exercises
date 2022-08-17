dcb9ca9c890ec126f2a6068a481eb7bb3c8dcfad
marcushoks <marcushoks@gmail.com>
2022-08-17 11:03:02 +0800

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (string, repeats) {
+  if (repeats < 0) return "ERROR";
 
+  const result = [];
+  for (let i = 0; i < repeats; i++) {
+    result.push(string);
+  }
+  return result.join("");
 };
 
 // Do not edit below this line
