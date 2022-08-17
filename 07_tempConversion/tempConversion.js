78eb1a54e9b2429aab2b7770f0fb9e3b77de1a93
marcushoks <marcushoks@gmail.com>
2022-08-17 13:48:33 +0800

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,19 @@
-const ftoc = function() {
-
+const round1DP = (num) => {
+  return Math.round(num * 10) / 10;
 };
 
-const ctof = function() {
+const ftoc = function (fahrenheit) {
+  let celcius = ((fahrenheit - 32) * 5) / 9;
+  return round1DP(celcius);
+};
 
+const ctof = function (celcius) {
+  let fahrenheit = (celcius * 9) / 5 + 32;
+  return round1DP(fahrenheit);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
