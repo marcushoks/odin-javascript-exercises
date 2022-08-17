cea33bd4b145382a361a3a83bc9074750f7ce0a8
marcushoks <marcushoks@gmail.com>
2022-08-17 13:27:43 +0800

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (typeof a !== "number" || typeof b !== "number") return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
 
+  let start;
+  let end;
+  if (a >= b) {
+    start = b;
+    end = a;
+  } else {
+    start = a;
+    end = b;
+  }
+
+  let result = 0;
+  for (let i = start; i <= end; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
