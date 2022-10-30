78182f7dec7e48ebf60c417987c7e7bfded08384
marcushoks <marcushoks@gmail.com>
2022-10-30 13:36:17 +0800

    Complete function findTheOldest()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const ages = people.reduce((array, { name, yearOfBirth, yearOfDeath }) => {
+    const age = (yearOfDeath ?? new Date().getFullYear()) - yearOfBirth;
+    array.push({ name, age });
+    return array;
+  }, []);
 
+  const oldestAge = Math.max(...ages.map(({ age }) => age));
+  return ages.find(({ age }) => age === oldestAge);
 };
 
 // Do not edit below this line
