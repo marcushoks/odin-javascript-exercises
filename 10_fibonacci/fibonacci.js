2baa450ad26abd4c98eef02cfc487260f777992d
marcushoks <marcushoks@gmail.com>
2022-10-30 12:13:40 +0800

    Complete function fibonacci()

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  const parsed = parseInt(num);
+  if (parsed <= 0) return "OOPS";
+  if (parsed === 1 || parsed === 2) return 1;
+  else return fibonacci(parsed - 1) + fibonacci(parsed - 2);
 };
 
 // Do not edit below this line
