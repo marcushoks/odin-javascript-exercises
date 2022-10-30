79e78dbcfcc121fe6463a7816901b2bda376f2e1
marcushoks <marcushoks@gmail.com>
2022-10-30 11:34:28 +0800

    Complete function factorial()

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,7 +18,13 @@ const power = function (a, b) {
   return a ** b;
 };
 
-const factorial = function () {};
+const factorial = function (num) {
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
+};
 
 // Do not edit below this line
 module.exports = {
