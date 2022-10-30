ed945b7c834f60fc3a057cca00a0d9ecc37c2ed7
marcushoks <marcushoks@gmail.com>
2022-10-30 11:53:00 +0800

    Complete function palindromes()

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const strToCompare = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => char.match(/[a-z]/));
 
+  const reversed = [...strToCompare].reverse();
+
+  return strToCompare.join("") === reversed.join("");
 };
 
 // Do not edit below this line
